94d39f1458c34846a6aa8c58543da23cfbb149e5
Uncle-Road <59357589+Uncle-Road@users.noreply.github.com>
2021-12-27 18:29:14 +0800

    Update mytest1.py

--- P2P_test/mytest1.py
+++ P2P_test/mytest1.py
@@ -4,18 +4,22 @@ from threading import Thread
 tracker_address = ("127.0.0.1", 10086)
 
 if __name__ == '__main__':
-    A = PClient(tracker_address, upload_rate=100, download_rate=100000, name="A")
-    B = PClient(tracker_address, upload_rate=100000, download_rate=1000, name="B")
-    C = PClient(tracker_address, upload_rate=100000, download_rate=50000, name="C")
-    D = PClient(tracker_address, upload_rate=100000, download_rate=100000, name="D")
+    A = PClient(tracker_address, upload_rate=1000000, download_rate=100000, name="A")
+    B = PClient(tracker_address, upload_rate=100000, download_rate=10000000, name="B")
+    C = PClient(tracker_address, upload_rate=100000, download_rate=500000000, name="C")
+    D = PClient(tracker_address, upload_rate=100000, download_rate=1000000000, name="D")
 
     fid = A.register("../test_files/alice.txt")
-
-    thread1 = Thread(target=B.download, args=fid)
-    thread2 = Thread(target=C.download, args=fid)
-    thread3 = Thread(target=D.download, args=fid)
-
-    thread1.start()
-    thread2.start()
-    thread3.start()
-
+    threads = []
+    files = {}
+    def download(node, index):
+        files[index] = node.download(fid)
+        # print(node.name, "foe:",files[ind
+    clients = [B, C, D]
+    for i, client in enumerate(clients):
+        threads.append(Thread(target=download, args=(client, i)))
+    for t in threads:
+        t.start()
+    # wait for finish
+    for t in threads:
+        t.join()

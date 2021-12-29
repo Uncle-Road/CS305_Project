67f1f6e5ee494aa3e7eea55f49196392c9fe3ef3
ak-maker <64005655+ak-maker@users.noreply.github.com>
2021-12-30 03:41:08 +0800

    Update ComplexTest.py

--- P2P_test/ComplexTest.py
+++ P2P_test/ComplexTest.py
@@ -5,25 +5,24 @@ from PClient import PClient
 
 tracker_address = ("127.0.0.1", 10086)
 
+
 if __name__ == '__main__':
     # A,B,C,D,E join the network
-    A = PClient(tracker_address, upload_rate=200000, download_rate=50000, name='A')
-    B = PClient(tracker_address, upload_rate=50000, download_rate=100000, name='B')
-    C = PClient(tracker_address, upload_rate=100000, download_rate=50000, name='C')
-    D = PClient(tracker_address, upload_rate=70000, download_rate=40000, name='D')
-    E = PClient(tracker_address, upload_rate=200000, download_rate=700000, name='E')
+    A = PClient(tracker_address, upload_rate=200000, download_rate=50000, name="A")
+    B = PClient(tracker_address, upload_rate=50000, download_rate=100000, name="B")
+    C = PClient(tracker_address, upload_rate=100000, download_rate=50000, name="C")
+    D = PClient(tracker_address, upload_rate=70000, download_rate=40000, name="D")
+    E = PClient(tracker_address, upload_rate=200000, download_rate=700000, name="E")
 
     clients = [B, C, D, E]
     # A register a file and B download it
-    fid = A.register("../test_files/alice.txt")
+    fid = A.register("../test_files/bg.png")
     threads = []
     files = {}
-
 
     # function for download and save
     def download(node, index):
         files[index] = node.download(fid)
-
 
     time_start = time.time_ns()
     for i, client in enumerate(clients):
@@ -35,7 +34,7 @@ if __name__ == '__main__':
     for t in threads:
         t.join()
     # check the downloaded files
-    with open("../test_files/alice.txt", "rb") as bg:
+    with open("../test_files/bg.png", "rb") as bg:
         bs = bg.read()
         for i in files:
             if files[i] != bs:
@@ -43,8 +42,8 @@ if __name__ == '__main__':
 
     # B, C, D, E has completed the download of file
     threads.clear()
-    F = PClient(tracker_address, upload_rate=50000, download_rate=100000, name='F')
-    G = PClient(tracker_address, upload_rate=100000, download_rate=60000, name='G')
+    F = PClient(tracker_address, upload_rate=50000, download_rate=100000, name="F")
+    G = PClient(tracker_address, upload_rate=100000, download_rate=60000, name="G")
     # F, G join the network
     clients = [F, G]
     for i, client in enumerate(clients):

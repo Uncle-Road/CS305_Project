65f070cd96ca8585ba76e95d82ac57fc8eb26e24
Uncle-Road <59357589+Uncle-Road@users.noreply.github.com>
2022-01-14 14:03:24 +0800

    Update Tracker.py

--- Tracker.py
+++ Tracker.py
@@ -7,10 +7,13 @@ class Tracker:
     def __init__(self, upload_rate=10000, download_rate=10000, port=10086):
         self.proxy = Proxy(upload_rate, download_rate, port)
         print("Tracker bind to", self.proxy.port)
-        self.files = {}  # ["fid":[('("127.0.0.1", 35555)', True),('("127.0.0.1", 41201)', True)]]
+        self.files = {}  # {"fid":[('("127.0.0.1", 35555)', True),('("127.0.0.1", 41201)', True)]}
+        # {"fid": {1:[('("127.0.0.1", 35555)', True), ('("127.0.0.1", 41201)', True)]},{2:}}
         self.tthread = Thread(target=self.listen)
         self.active = True
         self.files_now_available = {}
+        self.former = ""
+        self.have_files = set() #(fid,(127,11100))
 
     def __send__(self, data: bytes, dst: (str, int)):
         """
@@ -58,22 +61,30 @@ class Tracker:
             elif msg.startswith("QUERY:"):
                 # Client can use this to check who has the specific file with the given fid
                 fid = msg[7:]
-                result = []
-                # for c in self.files[fid]: #改变了数据结构,现在C为字典 test = {"fid":[("A",True),("B",True)]}
-                #     result.append(c)
-                for c in range(0, len(self.files[fid])):
-                    print(self.files[fid][c][1])
-                    if self.files[fid][c][1] is True:
-                        result.append(self.files[fid][c][0])  # ["("127.0.0.1",12)"]
-                        # c = (c[0], False)
-                        self.files[fid][c] = (self.files[fid][c][0], False)
-                        break
-                print(self.files)
-                print("tracker responds list: " + "[%s]" % (", ".join(result)))
-                self.response("LIST: " + "[%s]" % (", ".join(result)), frm)  # LIST: [A, B, C, D],frm
+                if (fid, frm) not in self.have_files:
+                    result = []
+                    # for c in self.files[fid]: #改变了数据结构,现在C为字典 test = {"fid":[("A",True),("B",True)]}
+                    #     result.append(c)
+                    for c in range(0, len(self.files[fid])):
+                        print(self.files[fid][c][1])
+                        if self.files[fid][c][1] is True:
+                            result.append(self.files[fid][c][0])  # ["("127.0.0.1",12)"]
+                            # c = (c[0], False)
+                            self.files[fid][c] = (self.files[fid][c][0], False)
+                            break
+                    print(self.files)
+
+
+                    if len(result) != 0:###########################3
+                        self.have_files.add((fid,frm))
+
+
+                    print("tracker responds list: " + "[%s]" % (", ".join(result)))
+                    self.response("LIST: " + "[%s]" % (", ".join(result)), frm)  # LIST: [A, B, C, D],frm
+
 
             elif msg.startswith("CANCEL:"):
-                 # Client can use this file to cancel the share of a file
+                # Client can use this file to cancel the share of a file
                 fid = msg[8:]
                 print("cancel")
                 if (client, True) in self.files[fid]:
@@ -81,6 +92,7 @@ class Tracker:
                 elif (client, False) in self.files[fid]:
                     self.files[fid].remove((client, False))
                 self.response("Success", frm)
+
 
             elif msg == "CLOSE":
                 # Client can use this to delete everything related to it
@@ -96,17 +108,13 @@ class Tracker:
                       self.files)  # now the files becomes {'../test_files/alice.txt': [('("127.0.0.1", 47201)', True), ('("127.0.0.1", 26488)', True)]}
 
 
+
             elif msg.startswith("Free"):
                 msg = msg[6:]
                 special_notion = re.search("-.", msg).span()
                 fid = msg[0:special_notion[0]]
                 change_true = msg[special_notion[1]:]  # 要free的IP和PORT
                 print("I am in Free now", fid, change_true)
-                # for c in self.files[fid]:
-                #     if c[0] is change_true and c[1] is False:
-                #         c = (c[0], True)
-                #     elif c[0] is change_true and c[1] is True:
-                #         raise Exception("Something wrong, this value should be false")
                 for c in range(0, len(self.files[fid])):
                     print("I am in For now not in if", self.files[fid][c][0], change_true,self.files[fid][c][1])
                     # if self.files[fid][c][0] is change_true and self.files[fid][c][1] is False:
